7714d98a131b8ed40010fa5839f26cd81a910b6f
una.zagala <vcaguilarg001@iesalbarregas.es>
2026-02-11 20:20:43 +0100

    clases

--- biblioteca/src/main/java/com/biblioteca/Biblioteca.java
+++ biblioteca/src/main/java/com/biblioteca/Biblioteca.java
@@ -1,6 +1,6 @@
 package com.biblioteca;
 
-public class Main {
+public class Biblioteca {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
---
+++ biblioteca/src/main/java/com/biblioteca/Prestamo.java
@@ -0,0 +1,5 @@
+package com.biblioteca;
+
+public class Prestamo {
+    
+}
