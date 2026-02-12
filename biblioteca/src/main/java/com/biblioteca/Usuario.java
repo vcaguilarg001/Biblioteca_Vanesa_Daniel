46cd8565404765e2ab018cef4efcba75698bdf82
danjc <danisito2003@gmail.com>
2026-02-12 21:23:53 +0100

    commit danie

--- biblioteca/src/main/java/com/biblioteca/Usuario.java
+++ biblioteca/src/main/java/com/biblioteca/Usuario.java
@@ -1,5 +1,35 @@
 package com.biblioteca;
+import java.util.Scanner;
 
 public class Usuario {
-    
+
+    static Scanner sc = new Scanner(System.in);
+
+    //Atributos
+    private String nombre;
+    private String contraseña;
+    private int prestamosActivos;
+    enum Rol {Usuario, Admin}
+
+    //Constructor usuario (todo está en construcción, es igual de desastre que mi vida)
+    public void crearUsuario(String nombre, String contraseña, Rol Usuario){
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+    }
+    //Constructor admin
+    public void crearAdmin(String nombre, String contraseña, Rol Admin){
+        this.nombre = nombre;
+        this.contraseña = contraseña;
+    }
+    public String getNombre(){
+        return nombre;
+    }
+
+    public int getPrestamosActivos(){
+        return prestamosActivos;
+    }
+    public void mostrarInfoUser(){
+        getNombre();
+        getPrestamosActivos();
+    }
 }
