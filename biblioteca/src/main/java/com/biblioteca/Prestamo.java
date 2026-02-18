505e5d8f9aeaacac02b83d98303251d800aa5065
danjc <danisito2003@gmail.com>
2026-02-18 19:34:15 +0100

    danisito clases

--- biblioteca/src/main/java/com/biblioteca/Prestamo.java
+++ biblioteca/src/main/java/com/biblioteca/Prestamo.java
@@ -1,5 +1,27 @@
 package com.biblioteca;
 
 public class Prestamo {
-    
+    private Usuario usuario;
+    private String libro;
+
+    public Prestamo(Usuario usuario, String libro){
+        this.usuario = usuario;
+        this.libro = libro;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public void setLibro(String libro) {
+        this.libro = libro;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public String getLibro() {
+        return libro;
+    }
 }
