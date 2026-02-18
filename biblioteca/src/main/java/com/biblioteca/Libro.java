fc02bdb41a93f43c0f2d1eb3173bd836b96ddcd0
una.zagala <vcaguilarg001@iesalbarregas.es>
2026-02-18 19:45:38 +0100

    Reconstrucción Libro

--- biblioteca/src/main/java/com/biblioteca/Libro.java
+++ biblioteca/src/main/java/com/biblioteca/Libro.java
@@ -2,4 +2,34 @@ package com.biblioteca;
 
 public class Libro {
     
+    private String titulo;
+    private String autor;
+    private String categoria;
+
+    public Libro(String titulo, String autor, String categoria) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.categoria = categoria;
+    }
+
+    // Getters
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public String getCategoria() {
+        return this.categoria;
+    }
+
+    // Funciones
+
+    @Override
+    public String toString() {
+        return "Titulo: " + this.titulo + " | Autor: " + this.autor + " | Categoria: " + this.categoria;
+    }
 }
